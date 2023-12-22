76ca7ee38a55c320d1e6b41339fb13649fac6831
vanjaandrejic <vanjaandrejic5@gmail.com>
2023-12-22 15:39:45 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>HELOO VANJA</h1>
+      <Button colorScheme="blue">BUTTOn</Button>
+    </>
+  );
+}
+
+export default App;
